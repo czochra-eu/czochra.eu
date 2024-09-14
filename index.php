64a62fcac6e64196682a1f8f9dcf64a322b54ceb
Prokrust Zombiakov <zombiakov@wp.pl>
2024-09-14 23:20:25 +0200

    Pierwsza strona. Wielka Czochra powraca.

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Wielka Czochra</title>
+</head>
+<body>
+Witajcie na naszej pierwszej stronie. Razem z nami podtrzymaj Ogień Wielkiej Czochry.
+<br/>
+<br/>
+v0.1
+</body>
+</html>
